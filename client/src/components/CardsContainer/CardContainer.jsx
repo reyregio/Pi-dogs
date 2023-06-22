874912d90dcb8e9491908c72fc1883e76e672c1a
reyregio <marisafenogliomnf@gmail.com>
2023-06-22 12:11:35 -0300

    card solucionado
    detalles

--- client/src/components/CardsContainer/CardContainer.jsx
+++ client/src/components/CardsContainer/CardContainer.jsx
@@ -5,21 +5,46 @@ import style from "./CardContainer.module.css";
 const CardContainer = ({ dogs }) => {
     return (
         <div className={style.container}>
-        {dogs.map((dog) => (
-            <Card
-            key={dog.id}
-            id={dog.id}
-            name={dog.name}
-            weight={dog.weight}
-            height={dog.height}
-            life_span={dog.life_span}
-            temperament={dog.temperament}
-            image={dog.image}
-            />
-            ))}
-            </div>
-            );
-        };
-        
-        export default CardContainer;
-        
+        {dogs.map((dog) => {
+            if (
+                dog.hasOwnProperty("weightMin") &&
+                dog.hasOwnProperty("weightMax") &&
+                dog.hasOwnProperty("heightMin") &&
+                dog.hasOwnProperty("heightMax") &&
+                dog.hasOwnProperty("life_spanMin") &&
+                dog.hasOwnProperty("life_spanMax")
+                ) {
+                    return (
+                        <Card
+                        key={dog.id}
+                        id={dog.id}
+                        name={dog.name}
+                        weight={`${dog.weightMin}-${dog.weightMax}`}
+                        height={`${dog.heightMin}-${dog.heightMax}`}
+                        life_span={`${dog.life_spanMin}-${dog.life_spanMax}`}
+                        temperament={dog.temperaments}
+                        image={dog.image}
+                        />
+                        );
+                    } else {
+                        return (
+                            <Card
+                            key={dog.id}
+                            id={dog.id}
+                            name={dog.name}
+                            weight={dog.weight}
+                            height={dog.height}
+                            life_span={dog.life_span}
+                            temperament={dog.temperaments}
+                            image={dog.image}
+                            />
+                            );
+                        }
+                    })}
+                    </div>
+                    );
+                };
+                
+                export default CardContainer;
+                
+                
